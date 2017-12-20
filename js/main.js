3624c38d16930ef1c4a7e8cf4f78b62acaa665ba
Dmitry <dmi.shevchenko@gmail.com>
2017-12-20 09:10:29 +0300

    industry page commit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,33 @@
 $('#why').click(function(event){
    $('#why-text').css('display','block')          
    $('#part-text').css('display','none')          
-   $('#corp-text').css('display','none')   
+   $('#corp-text').css('display','none')  
+    $('#why .line').css('opacity','1')
+    $('#part .line').css('opacity','0')
+    $('#corp .line').css('opacity','0')
              });
 $('#part').on('click', function(event){
    $('#why-text').css('display','none')          
    $('#part-text').css('display','block')          
-   $('#corp-text').css('display','none')     
+   $('#corp-text').css('display','none') 
+      $('#why .line').css('opacity','0')
+    $('#part .line').css('opacity','1')
+    $('#corp .line').css('opacity','0')
              });
 $('#corp').on('click', function(event){
    $('#why-text').css('display','none')          
    $('#part-text').css('display','none')          
-   $('#corp-text').css('display','block')          
+   $('#corp-text').css('display','block')  
+      $('#why .line').css('opacity','0')
+    $('#part .line').css('opacity','0')
+    $('#corp .line').css('opacity','1')
+             });
+
+$('#itm-1').on('click', function(event){         
+   $('.second-content').css('display','none')          
+   $('.first-content').css('display','block')          
+             });
+$('#itm-2').on('click', function(event){         
+   $('.first-content').css('display','none')          
+   $('.second-content').css('display','block')          
              });
